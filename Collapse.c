3257357d91334516b372b83ee15bbfea8e6dfb0f
OtroBenja <benja.gra2002@gmail.com>
2024-10-22 20:01:35 -0300

    Update Collapse.c
    Added normalization when computing the metric and a (still work in progress) K-essence model

--- Collapse.c
+++ Collapse.c
@@ -13,13 +13,14 @@ void omp_set_num_threads(){return;}
 
 #define MASS 0
 #define METRIC 1 // 0 = minkowski; 1 = choptuik; 2 = modified choptuik
-#define SAVE_MODE 1 // 0 = save uniformly on every SAVE_RES and SAVE_ITERATION ; 1 = save all points after FIRST_ITERATION and with r > MIN_R
+#define SAVE_MODE 0 // 0 = save uniformly on every SAVE_RES and SAVE_ITERATION ; 1 = save all points after FIRST_ITERATION and with r > MIN_R
 #define SAVE_RES 50
 #define SAVE_ITERATION 10
-#define FIRST_ITERATION 7700 //77950
+#define FIRST_ITERATION 77000 //77950
 #define MIN_R 0
 #define ITERATIONS 78500
 #define EPSILON 0.0 // Default Kreiss-Oliger dampening
+#define BETA 0.0
 #define PI 3.141592653
 #define E  2.718281828
 #define _6 0.166666666 // 1/6
@@ -64,7 +65,7 @@ double** initialize_field(int fType,double* model_parameters,double deltaR,int m
     double** return_values = malloc(sizeof(double*)*4);
 
     //Define r and calculate initial phi
-    r[0] = deltaR*0.1;
+    r[0] = 1.0E-50;
     for(int i=1;i<nR;i++)
         r[i] = i*deltaR;
     if(fType==0){
@@ -108,6 +109,50 @@ double** initialize_field(int fType,double* model_parameters,double deltaR,int m
     return return_values;
 }
 
+double RH_aK(double r, double a, double Phi2, double Pi2){
+    //double _a2 = 1/(a*a);
+    //double dif2 = Pi2 -Phi2;
+    //return a*( 4*PI*r*(Pi2*(1+BETA*dif2*_a2) -0.5*dif2*(1+0.5*BETA*_a2)) -0.5*(a*a-1)/r );
+    double X = 0.5*(Pi2-Phi2)/(a*a);
+    return a*( 4*PI*r*(Pi2*(1+2*BETA*X) -a*a*X*(1+BETA*X)) +0.5*(a*a-1)/r );
+}
+
+double RH_alphaK(double r, double a, double alpha, double Phi2, double Pi2){
+    //double _a2 = 1/(a*a);
+    //double dif2 = Pi2 - Phi2;
+    //return alpha*( 4*PI*r*(Phi2*(1+BETA*dif2*_a2) +0.5*dif2*(1+0.5*BETA*_a2)) +0.5*(a*a-1)/r );
+    double X = 0.5*(Pi2-Phi2)/(a*a);
+    return alpha*( 4*PI*r*(Phi2*(1+2*BETA*X) +a*a*X*(1+BETA*X)) +0.5*(a*a-1)/r );
+}
+
+double RH_PiK(double r, double a, double alpha, double Pi, double Phi, 
+              double* Epsilon, double* Gamma, int ir, double deltaR){
+    double _a2 = 1/(a*a);
+    double X = 0.5*(Pi*Pi-Phi*Phi)*_a2;
+    return ( centered_D1(Epsilon, ir, deltaR)/(r*r) + 16*BETA*PI*r*Gamma[ir]*Phi*Pi*X*(1+2*BETA*X)
+        +2*BETA*Phi*Pi*centered_D1(Gamma,ir,deltaR)*_a2 )/(1+2*BETA*X+2*BETA*Pi*Pi*_a2);
+
+}
+double RH_PiK_leftmid(double r, double a, double alpha, double Pi, double Phi, 
+              double* Epsilon, double* Gamma, int ir, double deltaR){
+    double _a2 = 1/(a*a);
+    double X = 0.5*(Pi*Pi-Phi*Phi)*_a2;
+    return ( leftmid_D1(Epsilon, ir, deltaR)/(r*r) + 16*BETA*PI*r*Gamma[ir]*Phi*Pi*X*(1+2*BETA*X)
+        +2*BETA*Phi*Pi*leftmid_D1(Gamma,ir,deltaR)*_a2 )/(1+2*BETA*X+2*BETA*Pi*Pi*_a2);
+
+}
+double RH_PiK_rightmid(double r, double a, double alpha, double Pi, double Phi, 
+              double* Epsilon, double* Gamma, int ir, double deltaR){
+    double _a2 = 1/(a*a);
+    double X = 0.5*(Pi*Pi-Phi*Phi)*_a2;
+    return ( rightmid_D1(Epsilon, ir, deltaR)/(r*r) + 16*BETA*PI*r*Gamma[ir]*Phi*Pi*X*(1+2*BETA*X)
+        +2*BETA*Phi*Pi*rightmid_D1(Gamma,ir,deltaR)*_a2 )/(1+2*BETA*X+2*BETA*Pi*Pi*_a2);
+
+}
+double RH_PiK_r0(double alpha, double Pi2, double* Phi, double deltaR){
+    return alpha*(1+BETA*Pi2)*leftmost_D1(Phi,0,deltaR);
+}
+
 void metric_iteration1(double* Beta, double* Beta1_2, double* a, double* alpha, double* r, int nR, double deltaR){
     
     int m1, n1, m2, n2, m3, n3, m4, n4;
@@ -130,7 +175,7 @@ void metric_iteration1(double* Beta, double* Beta1_2, double* a, double* alpha, 
     }
 }
 
-double** iteration(double* r,double* phi,double* Phi,double* Pi,double deltaR,int maxR,int iterations,int save_iteration,double epsilon){
+double** iteration_old(double* r,double* phi,double* Phi,double* Pi,double deltaR,int maxR,int iterations,int save_iteration,double epsilon){
 
     int nR = maxR/deltaR;
     int noSaveNR = MIN_R/deltaR;
@@ -486,7 +531,7 @@ double** iteration(double* r,double* phi,double* Phi,double* Pi,double deltaR,in
     return hist;
 }
 
-double** iteration2(double* r,double* phi,double* Phi,double* Pi,double deltaR,int maxR,int iterations,int save_iteration,double epsilon){
+double** iteration(double* r,double* phi,double* Phi,double* Pi,double deltaR,int maxR,int iterations,int save_iteration,double epsilon){
 
     int nR = maxR/deltaR;
     int noSaveNR = MIN_R/deltaR;
@@ -641,9 +686,11 @@ double** iteration2(double* r,double* phi,double* Phi,double* Pi,double deltaR,i
                 a[ir+1]     = a[ir]     +(m1 +2.0*(m2+m3) +m4)/6.0;
                 alpha[ir+1] = alpha[ir] +(n1 +2.0*(n2+n3) +n4)/6.0;
             }
+            temp = 1.0/(alpha[nR-1]*a[nR-1]);
             for(int ir=0;ir<nR;ir++){
-                Gamma[ir]   =        alpha[ir]*( Pi_rk[ir])/a[ir];
-                Epsilon[ir] = r2[ir]*alpha[ir]*(Phi_rk[ir])/a[ir];
+                alpha[ir]   = alpha[ir]*temp;
+                Gamma[ir]   =        alpha[ir]*( Pi_rk[ir])/(a[ir]);
+                Epsilon[ir] = r2[ir]*alpha[ir]*(Phi_rk[ir])/(a[ir]);
             }
             for(int ir=0;ir<5;ir++){
                 rPhi[ir] = r[nR-5 +ir]*Phi_rk[nR-5 +ir];
@@ -654,13 +701,10 @@ double** iteration2(double* r,double* phi,double* Phi,double* Pi,double deltaR,i
 
             j_n[0] = Gamma[0];
             k_n[0] = 0;
-            l_n[0] = leftmost_D1(Phi_rk,0,deltaR);
-            //l_n[0] = centered_r0_odd_D1(Phi_rk,deltaR); //Phi is odd
+            l_n[0] = alpha[0]*leftmost_D1(Phi_rk,0,deltaR);
             j_n[1] = Gamma[1];
             k_n[1] = leftmid_D1(Gamma,1,deltaR);
             l_n[1] = leftmid_D1(Epsilon,1,deltaR)/r2[1];
-            //k_n[1] = centered_r1_even_D1(Gamma,deltaR); //Gamma is even because Pi es even
-            //l_n[1] = centered_r1_odd_D1(Epsilon,deltaR)/r2[1]; //Epsilon is odd because Phi is odd
             //#pragma omp parallel for
             for(int ir=2;ir<nR-2;ir++){
                 j_n[ir] = Gamma[ir];
@@ -670,6 +714,228 @@ double** iteration2(double* r,double* phi,double* Phi,double* Pi,double deltaR,i
             j_n[nR-2] = Gamma[nR-2];
             k_n[nR-2] = rightmid_D1(Gamma,nR-2,deltaR);
             l_n[nR-2] = rightmid_D1(Epsilon,nR-2,deltaR)/r2[nR-2];
+            j_n[nR-1] = Gamma[nR-1];
+            k_n[nR-1] = -rightmost_D1(rPhi,4,deltaR)/r[nR-1];
+            l_n[nR-1] = -rightmost_D1( rPi,4,deltaR)/r[nR-1];
+
+            //Calculate phi, Phi and Pi on next step
+            for(int ir=0;ir<nR;ir++){
+                j_sum[ir] += rk[n]*j_n[ir];
+                k_sum[ir] += rk[n]*k_n[ir];
+                l_sum[ir] += rk[n]*l_n[ir];
+            }
+        }
+
+        //Calculate phi, Phi and Pi on next step
+        for(int ir=0;ir<nR;ir++){
+            phi[ir] += _6*deltaT*j_sum[ir];
+            Phi[ir] += _6*deltaT*k_sum[ir];//-ko_c*Phi_ko[ir];
+            Pi[ir]  += _6*deltaT*l_sum[ir];//-ko_c* Pi_ko[ir];
+        }
+    }
+    if(SAVE_MODE == 0){
+        for(int ir=0;ir<(nR/SAVE_RES);ir++)
+            Rhistory[ir] = r[ir*SAVE_RES];
+    }
+    else if(SAVE_MODE == 1){
+        for(int ir=0;ir<(saveNR);ir++)
+            Rhistory[ir] = r[noSaveNR+ir];
+    }
+    hist[0] = Rhistory;
+    hist[1] = Fhistory;
+    hist[2] = Xhistory;
+    hist[3] = Yhistory;
+    hist[4] = Ahistory;
+    hist[5] = Bhistory;
+    hist[6] = Mhistory;
+    return hist;
+}
+double** iterationK(double* r,double* phi,double* Phi,double* Pi,double deltaR,int maxR,int iterations,int save_iteration,double epsilon){
+
+    int nR = maxR/deltaR;
+    int noSaveNR = MIN_R/deltaR;
+    int saveNR = (maxR-MIN_R)/deltaR;
+    double deltaT = deltaR/5.;
+    double mass;
+    double *a = malloc(sizeof(double)*nR);
+    double *alpha = malloc(sizeof(double)*nR);
+    double *Phi2 = malloc(sizeof(double)*nR);
+    double *Pi2 = malloc(sizeof(double)*nR);
+    double *Phi2_p05 = malloc(sizeof(double)*nR);
+    double *Pi2_p05 = malloc(sizeof(double)*nR);
+    double *Gamma = malloc(sizeof(double)*nR);
+    double *Epsilon = malloc(sizeof(double)*nR);
+    double *Phi_rk = malloc(sizeof(double)*nR);
+    double * Pi_rk = malloc(sizeof(double)*nR);
+    //double *Phi_ko = malloc(sizeof(double)*nR);
+    //double *Pi_ko = malloc(sizeof(double)*nR);
+    double ko_c = pow(-1,3)*epsilon/5.;
+    double *r2 = malloc(sizeof(double)*nR);
+    double m1, n1, m2, n2, m3, n3, m4, n4;
+
+    double *rPhi = malloc(sizeof(double)*5);
+    double *rPi = malloc(sizeof(double)*5);
+    double *j_n = malloc(sizeof(double)*nR);
+    double *k_n = malloc(sizeof(double)*nR);
+    double *l_n = malloc(sizeof(double)*nR);
+    double *j_sum = malloc(sizeof(double)*nR);
+    double *k_sum = malloc(sizeof(double)*nR);
+    double *l_sum = malloc(sizeof(double)*nR);
+    double  rk[4] = {1.0,2.0,2.0,1.0};
+    double _rk[4] = {1.0,0.5,0.5,1.0};
+
+    double temp;
+    double *temp_pointer;
+    double *Rhistory, *Fhistory, *Xhistory, *Yhistory, *Ahistory, *Bhistory, *Mhistory;
+    if(SAVE_MODE == 0){
+        Rhistory = malloc(sizeof(double)*(nR/SAVE_RES));
+        Fhistory = malloc(sizeof(double)*(nR/SAVE_RES)*(iterations/save_iteration));
+        Xhistory = malloc(sizeof(double)*(nR/SAVE_RES)*(iterations/save_iteration));
+        Yhistory = malloc(sizeof(double)*(nR/SAVE_RES)*(iterations/save_iteration));
+        Ahistory = malloc(sizeof(double)*(nR/SAVE_RES)*(iterations/save_iteration));
+        Bhistory = malloc(sizeof(double)*(nR/SAVE_RES)*(iterations/save_iteration));
+        Mhistory = malloc(sizeof(double)*(iterations/save_iteration));
+    }
+    else if(SAVE_MODE == 1){
+        Rhistory = malloc(sizeof(double)*saveNR);
+        Fhistory = malloc(sizeof(double)*saveNR*(iterations-FIRST_ITERATION));
+        Xhistory = malloc(sizeof(double)*saveNR*(iterations-FIRST_ITERATION));
+        Yhistory = malloc(sizeof(double)*saveNR*(iterations-FIRST_ITERATION));
+        Ahistory = malloc(sizeof(double)*saveNR*(iterations-FIRST_ITERATION));
+        Bhistory = malloc(sizeof(double)*saveNR*(iterations-FIRST_ITERATION));
+        Mhistory = malloc(sizeof(double)*(iterations-FIRST_ITERATION));
+    }
+    double** hist = malloc(sizeof(double*)*7);
+    int save_count = save_iteration;
+
+    for(int ir=0;ir<nR;ir++){
+        a[ir] = 1.;
+        alpha[ir] = 1.;
+        r2[ir] = r[ir]*r[ir];
+    }
+    if(METRIC == 0){
+        for(int i=1;i<nR;i++){
+            //a[i] = 1.0 + 0.0001*pow(r[i],3)*pow(E,-pow((r[i]-10.0)/10,2));
+            //alpha[i] = pow(E,r[i]/100);
+        }
+    }
+
+    for(int i=0;i<iterations;i++){
+
+        if(SAVE_MODE == 0){
+            //Save values of Phi, Pi, phi, a and alpha
+            if(save_count == save_iteration){
+                //printf("iteration %d\n",i);
+                if(MASS){
+                    mass = 0;
+                    for(int ir=0;ir<nR;ir++){
+                        mass += 2*PI*r2[ir]*(Phi2[ir]+Pi2[ir])/(a[ir]*a[ir])*deltaR;
+                    }
+                    Mhistory[i/save_iteration] = mass;
+                }
+                for(int ir=0;ir<(nR/SAVE_RES);ir++){
+                    //Xhistory[(i/save_iteration)*(nR/SAVE_RES)+(ir)] = r[ir*SAVE_RES]*Phi[ir*SAVE_RES]*sqrt(2*PI)/a[ir*SAVE_RES];
+                    //Yhistory[(i/save_iteration)*(nR/SAVE_RES)+(ir)] = r[ir*SAVE_RES]* Pi[ir*SAVE_RES]*sqrt(2*PI)/a[ir*SAVE_RES];
+                    Xhistory[(i/save_iteration)*(nR/SAVE_RES)+(ir)] =   Phi[ir*SAVE_RES];
+                    Yhistory[(i/save_iteration)*(nR/SAVE_RES)+(ir)] =    Pi[ir*SAVE_RES];
+                    Fhistory[(i/save_iteration)*(nR/SAVE_RES)+(ir)] =   phi[ir*SAVE_RES];
+                    Ahistory[(i/save_iteration)*(nR/SAVE_RES)+(ir)] =     a[ir*SAVE_RES];
+                    Bhistory[(i/save_iteration)*(nR/SAVE_RES)+(ir)] = alpha[ir*SAVE_RES];
+                }
+                save_count=0;
+            }
+            save_count+=1;
+        }
+
+        if(SAVE_MODE == 1){
+            //Save values of Phi, Pi, phi, a and alpha
+            if(i >= FIRST_ITERATION){
+                int save_iter = i - FIRST_ITERATION;
+                //printf("iteration %d\n",i);
+                if(MASS){
+                    mass = 0;
+                    for(int ir=0;ir<nR;ir++){
+                        mass += 2*PI*r2[ir]*(Phi2[ir]+Pi2[ir])/(a[ir]*a[ir])*deltaR;
+                    }
+                    Mhistory[save_iter] = mass;
+                }
+                for(int ir=0;ir<saveNR;ir++){
+                    Xhistory[save_iter*saveNR + ir] =   Phi[noSaveNR + ir];
+                    Yhistory[save_iter*saveNR + ir] =    Pi[noSaveNR + ir];
+                    Fhistory[save_iter*saveNR + ir] =   phi[noSaveNR + ir];
+                    Ahistory[save_iter*saveNR + ir] =     a[noSaveNR + ir];
+                    Bhistory[save_iter*saveNR + ir] = alpha[noSaveNR + ir];
+                }
+            }
+        }
+
+        //Advance Pi and Phi using RK4 
+        for(int ir=0;ir<nR;ir++){
+            l_n[ir] = 0.0;
+            k_n[ir] = 0.0;
+            j_sum[ir] = 0.0;
+            k_sum[ir] = 0.0;
+            l_sum[ir] = 0.0;
+        }
+       //calculate jn, kn and ln
+        for(int n=0;n<4;n++){
+            for(int ir=0;ir<nR;ir++){
+                Phi_rk[ir] = Phi[ir]+_rk[n]*deltaT*k_n[ir];
+                 Pi_rk[ir] =  Pi[ir]+_rk[n]*deltaT*l_n[ir];
+                Phi2[ir] = (Phi_rk[ir])*(Phi_rk[ir]);
+                 Pi2[ir] = (Pi_rk[ir])*(Pi_rk[ir]);
+            }
+            for(int ir=0;ir<nR-1;ir++){
+                Phi2_p05[ir] = 0.25*(Phi_rk[ir] +Phi_rk[ir+1])*(Phi_rk[ir] +Phi_rk[ir+1]);
+                 Pi2_p05[ir] = 0.25*( Pi_rk[ir] + Pi_rk[ir+1])*( Pi_rk[ir] + Pi_rk[ir+1]);
+            }
+            //First iterate the metric for this Runge-Kutta step
+            for(int ir=0;ir<nR-1;ir++){
+                //calculate m1 and n1
+                m1 = deltaR*    RH_aK(r[ir],a[ir],          Phi2[ir],Pi2[ir]);
+                n1 = deltaR*RH_alphaK(r[ir],a[ir],alpha[ir],Phi2[ir],Pi2[ir]);
+                //calculate m2 and n2
+                m2 = deltaR*    RH_aK(r[ir]+0.5*deltaR,a[ir]+0.5*m1,                 Phi2_p05[ir],Pi2_p05[ir]);
+                n2 = deltaR*RH_alphaK(r[ir]+0.5*deltaR,a[ir]+0.5*m1,alpha[ir]+0.5*n1,Phi2_p05[ir],Pi2_p05[ir]);
+                //calculate m3 and n3
+                m3 = deltaR*    RH_aK(r[ir]+0.5*deltaR,a[ir]+0.5*m2,                 Phi2_p05[ir],Pi2_p05[ir]);
+                n3 = deltaR*RH_alphaK(r[ir]+0.5*deltaR,a[ir]+0.5*m2,alpha[ir]+0.5*n2,Phi2_p05[ir],Pi2_p05[ir]);
+                //calculate m4 and n4
+                m4 = deltaR*    RH_aK(r[ir+1],a[ir]+m3,             Phi2[ir+1],Pi2[ir+1]);
+                n4 = deltaR*RH_alphaK(r[ir+1],a[ir]+m3,alpha[ir]+n3,Phi2[ir+1],Pi2[ir+1]);
+                //Calculate next step for a and alpha
+                a[ir+1]     = a[ir]     +(m1 +2.0*(m2+m3) +m4)*_6;
+                alpha[ir+1] = alpha[ir] +(n1 +2.0*(n2+n3) +n4)*_6;
+            }
+            for(int ir=0;ir<nR;ir++){
+                Gamma[ir]   =        alpha[ir]*( Pi_rk[ir])/a[ir];
+                Epsilon[ir] = r2[ir]*alpha[ir]*(Phi_rk[ir])*(1+2*BETA*(Phi2[ir]-Pi2[ir]))/a[ir];
+            }
+            for(int ir=0;ir<5;ir++){
+                rPhi[ir] = r[nR-5 +ir]*Phi_rk[nR-5 +ir];
+                 rPi[ir] = r[nR-5 +ir]* Pi_rk[nR-5 +ir];
+            }
+
+
+            j_n[0] = Gamma[0];
+            k_n[0] = 0;
+            //l_n[0] = leftmost_D1(Phi_rk,0,deltaR);
+            l_n[0] = RH_PiK_r0(alpha[0],Pi2[0],Phi_rk,deltaR);
+            j_n[1] = Gamma[1];
+            k_n[1] = leftmid_D1(Gamma,1,deltaR);
+            //l_n[1] = leftmid_D1(Epsilon,1,deltaR)/r2[1];
+            l_n[1] = RH_PiK_leftmid(r[1],a[1],alpha[1],Pi_rk[1],Phi_rk[1],Epsilon,Gamma,1,deltaR);
+            //#pragma omp parallel for
+            for(int ir=2;ir<nR-2;ir++){
+                j_n[ir] = Gamma[ir];
+                k_n[ir] = centered_D1(Gamma,ir,deltaR);
+                //l_n[ir] = centered_D1(Epsilon,ir,deltaR)/r2[ir];
+                l_n[ir] = RH_PiK_leftmid(r[ir],a[ir],alpha[ir],Pi_rk[ir],Phi_rk[ir],Epsilon,Gamma,ir,deltaR);
+            }
+            j_n[nR-2] = Gamma[nR-2];
+            k_n[nR-2] = rightmid_D1(Gamma,nR-2,deltaR);
+            //l_n[nR-2] = rightmid_D1(Epsilon,nR-2,deltaR)/r2[nR-2];
+            l_n[nR-2] = RH_PiK_leftmid(r[nR-2],a[nR-2],alpha[nR-2],Pi_rk[nR-2],Phi_rk[nR-2],Epsilon,Gamma,nR-2,deltaR);
             j_n[nR-1] = Gamma[nR-1];
             k_n[nR-1] = -rightmost_D1(rPhi,4,deltaR)/r[nR-1];
             l_n[nR-1] = -rightmost_D1( rPi,4,deltaR)/r[nR-1];
@@ -850,7 +1116,7 @@ int main(int argc, char* argv[]){
     //Pass initial conditions to iteration
     if((argc>9) && atoi(argv[9])) omp_set_num_threads(atoi(argv[9]));
     time_t initTime = time(NULL);
-    hist = iteration2(r,phi,Phi,Pi,deltaR,maxR,iterations,SAVE_ITERATION,epsilon);
+    hist = iteration(r,phi,Phi,Pi,deltaR,maxR,iterations,SAVE_ITERATION,epsilon);
     time_t finalTime = time(NULL);
     int nP = omp_get_max_threads();
     time_t timeDelta = (finalTime-initTime);
